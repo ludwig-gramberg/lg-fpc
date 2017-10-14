cfb7b0077c584d6476d20b2e487a005c674ed899
ludwig <info@ludwig-gramberg.de>
2017-10-14 11:52:06 +0200

    bootstrap path

--- worker/cache_refresh.php
+++ worker/cache_refresh.php
@@ -4,7 +4,9 @@ namespace Lg;
 use Lg\FullPageCache\Config;
 use Lg\FullPageCache\Worker\CacheRefresh;
 
-require_once '../bootstrap.php';
+$bootstrapPath = rtrim(realpath(dirname(__FILE__).'/../'), '/').'/bootstrap.php';
+require_once $bootstrapPath;
+
 /** @var $config Config */
 
 $worker = new CacheRefresh($config, 'cache-refresh', 3, LG_FPC_REVISION_FILE, 128, 14400); // max 128MiB, 4 hours
